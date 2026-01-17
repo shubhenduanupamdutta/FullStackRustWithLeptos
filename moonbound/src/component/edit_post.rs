595f7261b5bb7ff8392fcaa22799d9eb15eef8ab
Shubhendu Anupam Dutta <shubhenduanupam@gmail.com>
2026-01-17 12:51:42 +0530

    Updates to edit post component

--- moonbound/src/component/edit_post.rs
+++ moonbound/src/component/edit_post.rs
@@ -1,6 +1,24 @@
 use leptos::prelude::*;
+use leptos_router::{hooks::use_params, params::Params};
+use serde::{Deserialize, Serialize};
+
+#[derive(Params, Eq, PartialEq, Debug, Clone, Serialize, Deserialize)]
+struct EditPostParams {
+    post_id: Option<String>,
+}
 
 #[component]
 pub fn EditPost() -> impl IntoView {
-    view! {}
+    let params = use_params::<EditPostParams>();
+    let display_params = move || {
+        match params.get() {
+            Ok(EditPostParams { post_id: Some(s)}) => s,
+            _ => "".to_string(),
+        }
+    };
+
+    view! {
+        {display_params}
+        
+    }
 }
